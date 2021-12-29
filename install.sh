dc32df26e406fbbb7e1b8cf081392cee0322e28d
Mattia Campolese <matsoftware@gmail.com>
2021-12-29 14:44:45 +0100

    Added support for M1 architecture (Issue#37)

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ python3 -m venv "${top}"/venv
 
 "${top}"/venv/bin/python3 "${top}"/venv/bin/pip3 install wheel
 
+if arch | grep -q 'arm64'; then
+    echo "Overriding pygraphviz setup for M1 architecture"
+    "${top}"/venv/bin/python3 "${top}"/venv/bin/pip3 install --global-option=build_ext --global-option="-I$(brew --prefix graphviz)/include" --global-option="-L$(brew --prefix graphviz)/lib" pygraphviz
+fi
+
 "${top}"/venv/bin/python3 "${top}"/venv/bin/pip3 install -r requirements.txt
 
 "${top}"/venv/bin/python3 "${top}"/venv/bin/pip3 install -r tests-requirements.txt
